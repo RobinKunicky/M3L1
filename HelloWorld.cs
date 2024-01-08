9c9eec5d04a0ba3b55d2fffb46891926d589799b
RobinKunicky <36965405+RobinKunicky@users.noreply.github.com>
2024-01-08 17:56:52 +0100

    ABCDEF
    jbsdfiuhjesoifbekfuioewb=wefjewmfiopaesklojf

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Output pre našu consolu
+        print("Hello World!");
+        print(2024);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
